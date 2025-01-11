57c8d4de86b252b733a21dfe701ae96c0f6929cc
Johan Hellberg <hellberg.johan@hotmail.com>
2025-01-11 23:53:28 +0100

    WIP login function

--- src/Login.tsx
+++ src/Login.tsx
@@ -1,6 +1,22 @@
 import './styles/App.css'
 import { NavLink } from 'react-router'
 
+/* 
+wip login
+https://docs.amplify.aws/angular/build-a-backend/auth/set-up-auth/
+
+<amplify-authenticator>
+  <ng-template
+    amplifySlot="authenticated"
+    let-user="user"
+    let-signOut="signOut"
+  >
+    <h1>Welcome {{ user.username }}!</h1>
+    <button (click)="signOut()">Sign Out</button>
+  </ng-template>
+</amplify-authenticator>
+*/
+
 function App() {
   return (
     <>
